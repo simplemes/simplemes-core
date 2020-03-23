9dfee2529f2b21eb0562a5f930a6a3f45eb4a743
mph <mphoust2@bellsouth.net>
2020-03-23 12:57:40 -0400

    Fixed issue with asset pipeline not being deployed in dev mode in sub-modules.

---
+++ eframe/src/main/groovy/org/simplemes/eframe/web/asset/EFrameAssetPipelineServiceGroovy.groovy
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Michael Houston 2020. All rights reserved.
+ */
+
+package org.simplemes.eframe.web.asset
+
+import io.micronaut.context.annotation.Primary
+import io.micronaut.context.annotation.Replaces
+
+import javax.inject.Singleton
+
+/**
+ * A groovy implementation of the asset pipeline replacement.  This just delegates
+ * to the .java class since the logic is not easily convertible to Groovy.
+ * <p>
+ * Micronaut does not handle beans defined in the Java tree along with the Groovy tree beans,
+ * so we just make sure all beans are define in Groovy (never in Java).
+ */
+@Primary
+@Singleton
+@Replaces(EFrameAssetPipelineService)
+class EFrameAssetPipelineServiceGroovy extends EFrameAssetPipelineService {
+
+}
