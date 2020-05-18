fb429514defe9bdbbc71db76e190c82715ee032d
mph <mphoust2@bellsouth.net>
2020-05-18 11:09:38 -0400

    Added delete component GUI test for product addition.

--- mes-assy/src/test/groovy/org/simplemes/mes/assy/product/domain/ProductAssemblyAdditionGUISpec.groovy
+++ mes-assy/src/test/groovy/org/simplemes/mes/assy/product/domain/ProductAssemblyAdditionGUISpec.groovy
@@ -81,6 +81,44 @@ class ProductAssemblyAdditionGUISpec extends BaseGUISpecification {
     comp.qty == 4.2
   }
 
+  def "verify that the product components can be deleted - edit mode"() {
+    given: 'a product with components'
+    def product = null
+    Product.withTransaction {
+      def comp1 = new Product(product: 'COMP1').save() as Product
+      def comp2 = new Product(product: 'COMP2').save() as Product
+      product = new Product(product: 'ABC')
+      def components = [new ProductComponent(component: comp1, sequence: 10, qty: 1.2),
+                        new ProductComponent(component: comp2, sequence: 20, qty: 2.2)]
+      product.setFieldValue('components', components)
+      product.save()
+    }
+
+    when: 'the edit page is displayed'
+    login()
+    to ProductEditPage, product
+
+    and: 'the components panel is displayed'
+    componentsPanel.click()
+
+    and: 'a row is removed'
+    components.cell(1, 0).click()
+    components.removeRowButton.click()
+
+    and: 'the record is saved'
+    updateButton.click()
+    waitForRecordChange(product)
+
+    then: 'the value is shown'
+    at ProductShowPage
+
+    and: 'the records are updated'
+    def record = Product.findByUuid(product.uuid)
+    def list = record.getFieldValue('components') as List<ProductComponent>
+    list.size() == 1
+    list[0].component.product == 'COMP1'
+  }
+
   def "verify that the assembly data type can be displayed and changed - edit mode"() {
     given: 'a product with an assembly data type'
     def product = null
